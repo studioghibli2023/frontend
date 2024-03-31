d83660bbdba19cce975b90f6f70700b9e5403bab
Adrian Iosif <146378579+L00179719@users.noreply.github.com>
2024-03-31 18:10:58 +0100

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
   private currentUsername = new BehaviorSubject<string | null>(localStorage.getItem('currentUsername'));
 
   //url: string = 'http://BE-studio-ghibli-load-balancer-1666703620.us-east-1.elb.amazonaws.com/app/'
-  url: string = 'http://localhost:8080/'
+  url: string = 'http://be-studio-ghibli-load-balancer-1609214022.us-east-1.elb.amazonaws.com/app/'
 
 
   constructor(private http: HttpClient) { }
